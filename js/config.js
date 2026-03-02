45dc86dce3063c8747cda39287458b9c191e1098
David Sánchez <david2sanchez@hotmail.com>
2026-03-02 10:53:59 +0100

    clientSecret

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'eu-central-1_tO5LSwOXM', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2a4bgo0823vmqej5ibsrqlaqug', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-central-1' // e.g. us-east-2
+        region: 'eu-central-1', // e.g. us-east-2
+		SECRET_HASH: '1s7o1v945eajajkssotskfnud6u0ovlej8ulg4hqr8bpfabpvchl'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
