9f84b551ae58e93d8c9912dbbca4e2ae5063b4b1
David Sánchez <david2sanchez@hotmail.com>
2026-03-02 11:20:47 +0100

    userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,9 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_tO5LSwOXM', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2a4bgo0823vmqej5ibsrqlaqug', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4300ids8bbu2sb9jj9as2v2a8u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1', // e.g. us-east-2
-		SECRET_HASH: '1s7o1v945eajajkssotskfnud6u0ovlej8ulg4hqr8bpfabpvchl'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
